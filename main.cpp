6f83c2d7336906149c0c7b852e28a54a35d6c743
u211-13 <u211-13@public.mpei.local>
2017-05-11 15:17:03 +0300

    2

--- main.cpp
+++ main.cpp
@@ -9,10 +9,17 @@ void print_byte(uint8_t byte){
     uint8_t hight = (byte & 0b11110000)>>4;
     cout<<nibble_to_hex(hight)<<nibble_to_hex(low);
 }
+void print_in_hex(const void* data, size_t size){
+    const uint8_t* p = reinterpret_cast<const uint8_t*>(data);
+    for(int i=0;i<size;i++){
+        print_byte(p[i]);
+    }
+
+}
 int
 main() {
-print_byte(42);
-
-
+    int n;
+    cin>>n;
+    print_in_hex(&n, sizeof(n));
     return 0;
 }
